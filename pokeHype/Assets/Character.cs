79b95282d73a416744c0b167168a9bb8fa11c745
Patryk Kowalik <patryk.kowalik@kapware.com>
2016-10-21 00:07:36 +0200

    project setup

---
+++ pokeHype/Assets/Character.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Character : MonoBehaviour {
+    Animator animator;
+    bool isDead, canAttack;
+    int hp, attackDamage;
+    float attackCooldown;
+    Character myTarget;
+    float rotateSpeed = 2;
+    float attackRange = 1.5f;
+    public bool isVisible { get; private set; }
+   
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(myTarget == null && isVisible)
+        {
+            Character[] characters = FindObjectsOfType<Character>();
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (characters[i] != this && characters[i].isVisible)
+                {
+                    myTarget = characters[i];
+                    return;
+                }
+            }
+        }
+        else if (TargetIsInRange() && !isDead)
+        {
+            RotateToTarget();
+            Attack();
+        }
+    }
+
+    private void RotateToTarget()
+    {
+        Vector3 targetDir = myTarget.transform.position - transform.position;
+        float step =  Time.deltaTime*rotateSpeed;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+
+    public void LostTarget()
+    {
+        isVisible = false;
+        myTarget = null;
+    }
+
+    public void ResetCharacter()
+    {
+        animator.Play("Wait");
+        isDead = false;
+        hp = 100;
+        attackDamage = UnityEngine.Random.Range(10, 30);
+        attackCooldown  = UnityEngine.Random.Range(0.5f, 3f);
+        canAttack = true;
+        animator.SetBool("isDead", isDead);
+        isVisible = true;
+    }
+
+    void Attack()
+    {
+        if(canAttack)
+        {
+            canAttack = false;
+            myTarget.GetDamage(attackDamage);
+            animator.SetTrigger("attack");
+            Invoke("UnlockAttack", attackCooldown);
+        }
+    }
+
+    private bool TargetIsInRange()
+    {
+        if(myTarget != null)
+        {
+            return Vector3.Distance(transform.position, myTarget.transform.position) <= attackRange;
+        }
+        return false;
+    }
+
+    public void GetDamage(int damage)
+    {
+        if (!isDead)
+        {
+            hp -= damage;
+            if (hp <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                animator.SetTrigger("getDamage");
+            }
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        animator.SetTrigger("dead");
+        animator.SetBool("isDead", isDead);
+    }
+
+    void UnlockAttack()
+    {
+        canAttack = true;
+    }
+}
---
+++ pokeHype/Assets/Scripts/ARCamera.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Vuforia;
+public class ARCamera : MonoBehaviour
+{
+    public static ARCamera instance;
+    bool successSet;
+    bool isSet;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    void Update()
+    {
+
+        if (!isSet && CameraDevice.Instance.GetCameraDirection() == CameraDevice.CameraDirection.CAMERA_BACK)
+        {
+            successSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+            if (successSet)
+                isSet = true;
+            else
+            {
+                successSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+                if (successSet)
+                    isSet = true;
+                else
+                {
+                    successSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_NORMAL);
+                    if (successSet)
+                        isSet = true;
+                }
+            }
+        }
+    }
+
+    private void OnApplicationPause(bool paused)
+    {
+        if (!paused)
+        {
+            // Set again autofocus mode when app is resumed
+            isSet = false;
+            successSet = false;
+        }
+    }
+
+    private void OnApplicationFocus(bool paused)
+    {
+        if (paused)
+        {
+            // Set again autofocus mode when app is resumed
+            isSet = false;
+            successSet = false;
+        }
+    }
+
+}
